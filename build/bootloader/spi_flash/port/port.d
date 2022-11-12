b9fec88cb25b15ab67f944ae3029004319da17e6
Joshua Elcock <joshua.elcock@gmail.com>
2022-11-12 01:48:26 +0000

    1st commit. Functionally similar to question 1.

---
+++ build/bootloader/spi_flash/port/port.d
@@ -0,0 +1,6 @@
+port/port.o: \
+ /opt/esp_sdk/ESP8266_RTOS_SDK/components/spi_flash/port/port.c \
+ /home/joshuaelcock/esp/Lab2P2b/build/bootloader/../include/sdkconfig.h
+
+/home/joshuaelcock/esp/Lab2P2b/build/bootloader/../include/sdkconfig.h:
+/opt/esp_sdk/ESP8266_RTOS_SDK/components/spi_flash/port/port.c:
